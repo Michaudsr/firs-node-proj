5521b327d5d381d84013ec69f39ae5dc51827f20
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2020-08-03 19:06:01 -0400

    [node] first node project

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+// node keyword require allows us to import modules
+// const myModule = require('./myModule.js')
+// const fs = require('fs')
+
+// fs.readFile("story.txt", "utf8", (err, data)=>{
+//     if(err) {
+//         console.log("There was a problem reading the file")
+//     } else {
+//         console.log(data)
+//     }
+// })
+
+// console.log(myModule.beBasic())
+
+// myModule.count()
+
+// console.log('hello, world!')
+// let i = 0
+
+// const count = () => {
+//     console.log(i)
+//     i++
+// }
+
+// const myTimer = setInterval(count, 1000)
+
+// const today = new Date ()
+
+// console.log(today)
+// const moment = require('moment')
+
+// console.log(moment().format("MM Do YYYY"))
+
+
+// console.log(moment().format("LLLL"))
+
+console.log(moment("3/10/1993", "MM DD YYYY").format("dddd [the] Do [of] MMM [in the year] YYYY"));
